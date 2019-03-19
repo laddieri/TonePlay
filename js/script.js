80a134fb89052deabf3df986b1c0f6874266230d
laddieri <laddieri@gmail.com>
2019-03-18 20:30:36 -0400

    added rit

--- js/script.js
+++ js/script.js
@@ -3,26 +3,47 @@ document.documentElement.addEventListener(
     mouse_IsDown = true;
     if (Tone.context.state !== 'running') {
     Tone.context.resume();
+    Tone.Transport.bpm.value =145;
 
 }});
 
 var box = document.querySelector(".flashbox");
 
-
+var beatcount =0;
+var barcount=1;
 var player = new Tone.Player("./sounds/clap.wav").toMaster();
+var ritardandoTrue=false;
 
 function triggerSound(time){
 	player.start()
+  beatcount+=1;
+  console.log(Tone.Transport.bpm.value)
+  console.log(ritardandoTrue);
+
+  if (ritardandoTrue){
+    if (barcount==3 ){
+      Tone.Transport.bpm.value = ritardando(beatcount);
+    }
+    if(barcount==4 && beatcount==1){
+      Tone.Transport.bpm.value = ritardando(5);
+    }
+    if (beatcount==4){
+      barcount+=1;
+      beatcount=0;
+    }
+  }
+
+}
+
+function ritardando(x){
+  y=-11*x + 145;
+  return y;
 }
 
 Tone.Transport.schedule(function(time){
   triggerSound(time)
   Tone.Draw.schedule(function(){
-    //the callback synced to the animation frame at the given time
-    box.classList.add("active");
-    setTimeout(() => {
-      box.classList.remove("active");
-    }, 100);
+      document.querySelector('tone-slider').value=Tone.Transport.bpm.value;
   })
 }, 0)
 Tone.Transport.loop = true;
@@ -35,9 +56,11 @@ document.querySelector('tone-play-toggle').addEventListener('change', e => Tone.
 //start/stop the transport
 document.querySelector('tone-slider').addEventListener('change', e => Tone.Transport.bpm.value = e.detail)
 
+document.querySelector('tone-toggle').addEventListener('change', e=>ritardandoTrue=!ritardandoTrue)
+
 function updateTime() {
 requestAnimationFrame(updateTime)
-  document.querySelector('span').textContent = Tone.context.currentTime.toFixed(3)
+  // document.querySelector('span').textContent = Tone.context.currentTime.toFixed(3)
 }
 
 updateTime()
