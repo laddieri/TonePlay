04df9ce4edd7b1f7b6bec24875c7b75f1f8ec254
laddieri <laddieri@gmail.com>
2019-05-10 06:14:33 -0400

    Added visualization removed ritartando

--- js/script.js
+++ js/script.js
@@ -1,50 +1,32 @@
+var xpos=0;
+var rad=50;
+var t=0;
+var secondsPerBeat;
+var bpm = 60;
+
+
 document.documentElement.addEventListener(
   "mousedown", function(){
     mouse_IsDown = true;
     if (Tone.context.state !== 'running') {
     Tone.context.resume();
-    Tone.Transport.bpm.value =145;
-
+    Tone.Transport.bpm.value = 60;
 }});
 
-var box = document.querySelector(".flashbox");
 
-var beatcount =0;
-var barcount=1;
 var player = new Tone.Player("./sounds/clap.wav").toMaster();
-var ritardandoTrue=false;
 
 function triggerSound(time){
 	player.start()
-  beatcount+=1;
-  console.log(Tone.Transport.bpm.value)
-  console.log(ritardandoTrue);
-
-  if (ritardandoTrue){
-    if (barcount==3 ){
-      Tone.Transport.bpm.value = ritardando(beatcount);
-    }
-    if(barcount==4 && beatcount==1){
-      Tone.Transport.bpm.value = ritardando(5);
-    }
-    if (beatcount==4){
-      barcount+=1;
-      beatcount=0;
-    }
-  }
-
-}
-
-function ritardando(x){
-  y=-11*x + 145;
-  return y;
 }
 
 Tone.Transport.schedule(function(time){
   triggerSound(time)
   Tone.Draw.schedule(function(){
       document.querySelector('tone-slider').value=Tone.Transport.bpm.value;
-  })
+      t=0;
+      })
+
 }, 0)
 Tone.Transport.loop = true;
 Tone.Transport.loopEnd = '4n';
@@ -56,11 +38,30 @@ document.querySelector('tone-play-toggle').addEventListener('change', e => Tone.
 //start/stop the transport
 document.querySelector('tone-slider').addEventListener('change', e => Tone.Transport.bpm.value = e.detail)
 
-document.querySelector('tone-toggle').addEventListener('change', e=>ritardandoTrue=!ritardandoTrue)
-
 function updateTime() {
 requestAnimationFrame(updateTime)
-  // document.querySelector('span').textContent = Tone.context.currentTime.toFixed(3)
+  secondsPerBeat=1/(Tone.Transport.bpm.value/60);
+}
+
+function setup() {
+  var xwidth=640
+  var yheight=480;
+  createCanvas(xwidth, yheight);
+  frameRate(60);
+  xpos=0+rad;
+}
+
+function draw() {
+  background(255);
+  ellipse(xpos, 50, rad, rad);
+  xpos = updateXpos(xpos);
+  t++;
+}
+
+function updateXpos(xpos){
+
+  xpos=1000/(secondsPerBeat*60)*(t-((1/(secondsPerBeat*60))*t*t));
+  return xpos;
 }
 
 updateTime()
